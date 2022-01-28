39fabaf890cf32bd877aab508f6e59ff23d917f0
Ana Matei <anamatei.099@gmail.com>
2022-01-28 15:21:22 +0000

    Solved Exercise 7

--- homeworkM1D5.js
+++ homeworkM1D5.js
@@ -88,7 +88,37 @@ console.log (final)
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR ANSWER HERE */
+printHeader ("EXERCISE 7")
+
+
+    // Step 1. Create an empty string that will host the new created string
+ 
+    // Step 2. Create the FOR loop
+    /* The starting point of the loop will be (str.length - 1) which corresponds to the 
+       last character of the string, "o"
+       As long as i is greater than or equals 0, the loop will go on
+       We decrement i after each iteration */
+    /* Here hello's length equals 5
+        For each iteration: i = str.length - 1 and newString = newString + str[i]
+        First iteration:    i = 5 - 1 = 4,         newString = "" + "o" = "o"
+        Second iteration:   i = 4 - 1 = 3,         newString = "o" + "l" = "ol"
+        Third iteration:    i = 3 - 1 = 2,         newString = "ol" + "l" = "oll"
+        Fourth iteration:   i = 2 - 1 = 1,         newString = "oll" + "e" = "olle"
+        Fifth iteration:    i = 1 - 1 = 0,         newString = "olle" + "h" = "olleh"
+    End of the FOR Loop*/
+ 
+    // Step 3. Return the reversed string
+    
+
+function reverseString(str) {
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];// or newString = newString + str[i];
+    }
+    return newString;
+}
+reverseString('hello');
+console.log (reverseString)
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
