0d5bfe054fcf72dc43fc6dfcd101852d1519cbd3
Ana Matei <anamatei.099@gmail.com>
2022-01-28 15:39:20 +0000

    Update Exercise 3

--- homeworkM1D5.js
+++ homeworkM1D5.js
@@ -63,6 +63,9 @@ function crazyDiff (a)
 
 let a = 23
 console.log ("\nThe final result is: ", crazyDiff(a))
+let c = 23
+console.log ("\nThe final result is: ", crazyDiff(c))
+
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
